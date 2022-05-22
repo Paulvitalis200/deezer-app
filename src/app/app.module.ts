c83e22aced9054ed23916d0f9fadec2cfe9aa0ff
Paul Otieno <vitalispaul48@live.com>
2022-05-22 09:20:03 +0300

    Style search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { NavigationComponent } from './components/navigation/navigation.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    SharedModule
+    // MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtistsService } from 'src/app/core/services/artists/artists.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  artist: string = ''
+  constructor(private artistsService: ArtistsService) { }
 
   ngOnInit(): void {
+    this.getArtists()
+  }
+
+  getArtists() {
+    this.artistsService.getArtists(this.artist).subscribe((res: any) => {
+      console.log("RES: ", res)
+    },
+      (err) => {
+        console.log("Error: ", err)
+      })
+  }
+
+  applyFilter(event: Event): void {
+    const filter = (event.target as HTMLInputElement).value.trim().toLocaleLowerCase();
+    this.artist = filter
+    this.getArtists()
+    console.log("FILTER: ", filter)
   }
 
 }
